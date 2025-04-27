0461e0cfb5c7e603631c49099485a15bc4d4a8e1
manish3299 <ambarwarm1234@gmail.com>
2025-04-27 23:08:59 +0530

    Refactor : Done changes to index.ts according to  components for scalable exports

--- src/index.ts
+++ src/index.ts
@@ -1,101 +1,20 @@
-
-
-// Button
-import Button from "./components/button/Button";
-export type { ButtonProps } from "./components/button/Button";
-export { Button };
-
-// Typography
-import Typography from "./components/typography/Typography";
-export type { TypographyProps } from "./components/typography/Typography";
-export { Typography };
-
-// Input
-import Input from "./components/input/Input";
-export type { InputProps } from "./components/input/Input";
-export { Input };
-
-// Textarea
-import Textarea from "./components/textarea/Textarea";
-export type { TextareaProps } from "./components/textarea/Textarea";
-export { Textarea };
-
-// Checkbox
-import Checkbox from "./components/checkbox/Checkbox";
-export type { CheckboxProps } from "./components/checkbox/Checkbox";
-export { Checkbox };
-
-// RadioGroup
-import RadioGroup from "./components/radio-group/RadioGroup";
-export type { RadioGroupProps } from "./components/radio-group/RadioGroup";
-export { RadioGroup };
-
-// Switch
-import Switch from "./components/switch/Switch";
-export type { SwitchProps } from "./components/switch/Switch";
-export { Switch };
-
-// Select
-import Select from "./components/select/Select";
-export type { SelectProps } from "./components/select/Select";
-export { Select };
-
-// Spinner
-import { Spinner } from "./components/spinner/Spinner";
-//export type { SpinnerProps } from './components/spinner/Spinner';
-export { Spinner };
-
-// ProgressBar
-import ProgressBar from "./components/progress-bar/ProgressBar";
-export type { ProgressBarProps } from "./components/progress-bar/ProgressBar";
-export { ProgressBar };
-
-// Badge
-import Badge from "./components/badge/Badge";
-export type { BadgeProps } from "./components/badge/Badge";
-export { Badge };
-
-// Alert
-import Alert from "./components/alert/Alert";
-export type { AlertProps } from "./components/alert/Alert";
-export { Alert };
-
-// Toast
-import Toast from "./components/toast/Toast";
-export type { ToastProps } from "./components/toast/Toast";
-export { Toast };
-
-// Avatar
-import Avatar from "./components/avatar/Avatar";
-export type { AvatarProps } from "./components/avatar/Avatar";
-export { Avatar };
-
-// Breadcrumbs
-import Breadcrumbs from "./components/breadcrumbs/Breadcrumbs";
-export type { BreadcrumbsProps } from "./components/breadcrumbs/Breadcrumbs";
-export { Breadcrumbs };
-
-// Menu
-import Menu from "./components/menu/Menu";
-export type { MenuProps } from "./components/menu/Menu";
-export { Menu };
-
-// Slider
-import Slider from "./components/slider/Slider";
-export type { SliderProps } from "./components/slider/Slider";
-export { Slider };
-
-// Stepper
-import Stepper from "./components/stepper/Stepper";
-export type { StepperProps } from "./components/stepper/Stepper";
-export { Stepper };
-
-// Pagination
-import Pagination from "./components/pagination/Pagination";
-export type { PaginationProps } from "./components/pagination/Pagination";
-export { Pagination };
-
-// Table
-import Table from "./components/table/Table";
-export type { TableProps } from "./components/table/Table";
-export { Table };
+export * from "./components/alert";
+export * from "./components/avatar";
+export * from "./components/badge";
+export * from "./components/breadcrumbs";
+export * from "./components/button";
+export * from "./components/checkbox";
+export * from "./components/input";
+export * from "./components/menu";
+export * from "./components/pagination";
+export * from "./components/progress-bar";
+export * from "./components/radio-group";
+export * from "./components/select";
+export * from "./components/slider";
+export * from "./components/spinner";
+export * from "./components/stepper";
+export * from "./components/switch";
+export * from "./components/table";
+export * from "./components/textarea";
+export * from "./components/toast";
+export * from "./components/typography";
